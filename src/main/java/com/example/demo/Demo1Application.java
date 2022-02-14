91e22be808aab932004529a522b0fa040fe25f0b
Ubuntu <ubuntu@ip-172-31-35-189.ap-south-1.compute.internal>
2022-02-14 11:41:53 +0000

    Modified output

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -13,7 +13,7 @@ public class Demo1Application {
 	@RequestMapping("/")
 	@ResponseBody
 	String home() {
-		return "Greetings from Nuclear Geeks QA";
+		return "Hello all, Welcome to Shark tank";
 	}
 
 
